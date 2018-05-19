227a4c697891060aa536432d3d9b926dee6c3830
Owner <j94679@gmail.com>
2018-05-19 07:35:08 -0300

    Schoooool

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private EditText inputText;
+    private TextView textOut;
+
+    public void buttonOnClick(View v)
+    {
+        Button button=(Button) v;
+        inputText = (EditText) findViewById(R.id.inputText);
+        textOut = (TextView) findViewById(R.id.textOut);
+        textOut.setText(inputText.getText());
+    }
 }
+
